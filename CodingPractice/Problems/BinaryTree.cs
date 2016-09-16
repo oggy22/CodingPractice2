30f4eea16a7104d1ac94b2d7b25dbed6a127aef2
oggy22@gmail.com <oggy22@gmail.com>
2016-09-15 21:57:39 -0700

    Adding more problems

---
+++ CodingPractice/Problems/BinaryTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPractice.Problems
+{
+    public class BinaryTreeNode
+    {
+        public int data;
+        public BinaryTreeNode left, right;
+        BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    [TestClass]
+    public class BinaryTree : ProblemBase
+    {
+        public override string Description => "Check if the given binary tree is a binary SEARCH tree.";
+
+        public override Type Input
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Type Output
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string Title => "Binary Tree";
+
+        public bool IsBinarySearchTree(BinaryTreeNode root)
+        {
+            return RecursiveCheck(root, null, null);
+        }
+
+        private bool RecursiveCheck(BinaryTreeNode node, int? leftLimit, int? rightLimit)
+        {
+            if (leftLimit != null && leftLimit >= node.data)
+                return false;
+
+            if (rightLimit != null && rightLimit <= node.data)
+                return false;
+
+            if (node.left != null)
+                if (!RecursiveCheck(node.left, leftLimit, node.data))
+                    return false;
+
+            if (node.right != null)
+                if (!RecursiveCheck(node.right, node.data, rightLimit))
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ CodingPractice/Problems/EmailRegex.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace CodingPractice.Problems
+{
+    [TestClass]
+    public class EmailRegex : ProblemBase
+    {
+        public override string Description => "Email regex";
+
+        public override Type Input => typeof(string);
+
+        public override Type Output => typeof(bool);
+
+        public override string Title => "Email regex";
+
+        public bool solve(string stEmail)
+        {
+            Regex regex = new Regex("[A-Za-z_][A-Za-z0-9_]+@[A-Za-z0-9_]+\\.[A-Za-z0-9_]+");
+            MatchCollection mc = regex.Matches(stEmail);
+            for (int i = 0; i<mc.Count; i++)
+            {
+                Match m = mc[i];
+                if (m.Length == stEmail.Length)
+                    return true;
+            }
+            return false;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(solve("oggy@gmail.com"));
+            Assert.IsFalse(solve("oggy@gmail"));
+            Assert.IsFalse(solve("oggy@gmail@oggy"));
+            Assert.IsFalse(solve("oggy@gmail.com@oggy"));
+            return;
+
+            Assert.IsTrue(solve("ognjen.sobajic@gmail.com"));
+        }
+    }
+}
